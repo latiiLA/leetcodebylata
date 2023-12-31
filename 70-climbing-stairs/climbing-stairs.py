a5ee45e740caff43e6e42093b9817a6ca1f8b1a7
latiiLA <49594109+latiiLA@users.noreply.github.com>
2023-12-31 12:05:04 +0300

    Time: 25 ms (98.75%) | Memory: 17.3 MB (9.73%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -1,10 +1,13 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
+        if n == 0 or n == 1:
+            return 1
         result = [0] * (n+1)
         result[0] = 1
         result[1] = 1
+        result[2] = 2
         print(result)
-        for i in range(2, n+1):
+        for i in range(3, n+1):
             result[i] = result[i-1] + result[i-2]
             print(result)
-        return result[n]
+        return result[-1]
